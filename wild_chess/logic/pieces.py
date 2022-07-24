39765294edbc17033e3a3b69f3b5878e27e7da61
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-07-24 18:08:47 +0530

    added chess pieces class

--- wild_chess/logic/pieces.py
+++ wild_chess/logic/pieces.py
@@ -1,20 +1,79 @@
 """All pieces are defined here"""
-from typing import List
+from typing import List, Dict, Union, Tuple
 
-from wild_chess.utils.piece import Piece
 
 # pylint: disable=R0913
 # pylint: disable=R0903
 
 
-class Pawn(Piece):
+class ChessPiece:
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        self.position = position
+        self.player = player
+
+
+class Pawn(ChessPiece):
     """The pawn piece"""
+    piece_type: str = "Pawn"
+    takeable: bool = True
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Forward"], "Limit": 1}
 
-    def __init__(self, team: str, piece_type: str, takeable: bool, image: str, possible_moves: List) -> None:
-        super().__init__(team, piece_type, takeable, image, possible_moves)
-        self.possible_moves = possible_moves
-        self.has_moved = False
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
 
-    def movement(self) -> None:
-        """Handles movement of pieces"""
-        self.possible_moves = [1, 1]
+
+class Rook(ChessPiece):
+    """The rook piece"""
+    piece_type: str = "Rook"
+    takeable: bool = True
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Forward", "Sides"], "Limit": None}
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
+
+
+class Knight(ChessPiece):
+    """The knight piece"""
+    piece_type: str = "Knight"
+    takeable: bool = True
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Crooked-Diagonal"], "Limit": 1}
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
+
+
+class Bishop(ChessPiece):
+    """The bishop piece"""
+    piece_type: str = "Bishop"
+    takeable: bool = True
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Diagonal"], "Limit": None}
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
+
+
+class Queen(ChessPiece):
+    """The queen piece"""
+    piece_type: str = "Queen"
+    takeable: bool = True
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Forward", "Sides", "Diagonal"], "Limit": None}
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
+
+
+class King(ChessPiece):
+    """The king piece"""
+    piece_type: str = "King"
+    takeable: bool = False
+    image: str = ""
+    possible_moves: Dict[str, Union[int, List[str]]] = {"Direction": ["Forward", "Sides", "Diagonal"], "Limit": 1}
+
+    def __init__(self, position: Tuple[int, int], player: str) -> None:
+        super().__init__(position, player)
--- wild_chess/utils/piece.py
+++ wild_chess/utils/piece.py
@@ -1,16 +1,19 @@
 """A piece class, the parent of all pieces"""
-from typing import List
+from typing import List, Tuple
 
 # pylint: disable=R0913
 # pylint: disable=R0903
 
+# create a class board and subclass it to have two teams
 
-class Piece:
+
+class Board:
+    ...
+
+
+class Team:
     """Dummy piece class, to be edited later and as required"""
 
-    def __init__(self, team: str, piece_type: str, takeable: bool, image: str, possible_moves: List) -> None:
-        self.team = team  # this variable also controls the color
-        self.piece_type = piece_type
-        self.takeable = takeable  # if a piece is a king it can't be taken
-        self.image = image
-        self.possible_moves = possible_moves
+    def __init__(self, player: str) -> None:
+        self.player = player  # this variable also controls the color
+        self.position = []  # this variable stores the position of the piece in a team
