609d49386066732aedf5c17b8270a663e3a558a6
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 01:27:39 +0530

    Fix linter type issues

--- wild_chess/database/postgres.py
+++ wild_chess/database/postgres.py
@@ -1,42 +1,55 @@
-import typing as t
+"""Base database model."""
+
+
+import typing
 
 import asyncpg
 
-__all__: t.Tuple[str, ...] = ("DatabaseModel",)
+__all__: tuple[str, ...] = ("DatabaseModel",)
 
 
 class DatabaseModel:  # model commands class for database
-    """
-    General methods for database.
-    """
+    """General methods for database."""
 
     pool: asyncpg.pool.Pool
 
-    async def exec_write_query(self, query: str, data: t.Tuple[t.Any, ...] | None = None) -> None:
+    async def exec_write_query(self, query: str, data: typing.Optional[tuple] = None) -> None:
         """
         Execute a write query.
+
         :param query:
+        :type query: str
         :param data:
-        :return:
+        :type data: typing.Optional[tuple]
         """
         if data:
             await self.pool.execute(query, *data)
             return
+
         await self.pool.execute(query)
 
-    async def exec_fetchone(self, query: str, data: t.Tuple[t.Any, ...] | None = None) -> t.Optional[asyncpg.Record]:
+    async def exec_fetchone(self, query: str, data: typing.Optional[tuple] = None) -> typing.Optional[asyncpg.Record]:
         """
         Execute a fetchone query.
-        :param query:
-        :param data:
-        :return:
-        """
-        return await self.pool.fetchrow(query, *data)
 
-    async def exec_fetchall(self, query: str) -> t.List[asyncpg.Record]:
+        :param query:
+        :type query: str
+        :param data:
+        :type data: typing.Optional[tuple]
+        :return:
+        :rtype: typing.Optional[asyncpg.Record]
+        """
+        result: typing.Optional[asyncpg.Record] = await self.pool.fetchrow(query, *data)
+        return result
+
+    async def exec_fetchall(self, query: str) -> list[asyncpg.Record]:
         """
         Execute a fetchall query.
+
         :param query:
+        :type query: str
         :return:
+        :rtype: list[asyncpg.Record]
         """
-        return await self.pool.fetch(query)
+        results: list[asyncpg.Record] = await self.pool.fetch(query)
+        return results
