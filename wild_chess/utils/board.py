0b1013c6fa1bddf6a768ba19088efbfb77250f8f
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 01:56:42 +0530

    Partially clean the code

--- wild_chess/utils/board.py
+++ wild_chess/utils/board.py
@@ -1,54 +1,67 @@
 """A piece class, the parent of all pieces"""
-from typing import List, Optional
-
-from ..logic import Bishop, ChessPiece, King, Knight, Pawn, Queen, Rook
-from ..utils import PlayerAttributes
-
-# pylint: disable=R0913
-# pylint: disable=R0903
-
-# create a class board and subclass it to have two teams
 
 
+import typing
+
+from wild_chess.logic import pieces
+from wild_chess.utils import data
+
+
+# Create a class board and subclass it to have two teams
 class Board:
-    board: List[List[Optional[ChessPiece]]]
+    """A chess board."""
+
+    board: list[list[typing.Optional[pieces.ChessPiece]]]
 
     def __init__(self) -> None:
-        self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.board = [
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+        ]
 
-    def generate_pieces(self, player1: PlayerAttributes, player2: PlayerAttributes) -> None:
+    # TODO: Refactor this method
+    def generate_pieces(self, player1: data.PlayerAttributes, player2: data.PlayerAttributes) -> None:
         """
         Generate the pieces for the board.
+
         :param player1:
         :param player2:
         :return:
         """
         for i in range(8):
-            self.board[1][i] = Pawn((1, i), player1.name, player1.color)
-            self.board[6][i] = Pawn((6, i), player2.name, player2.color)
-        self.board[0][0] = Rook((0, 0), player1.name, player1.color)
-        self.board[0][7] = Rook((0, 7), player1.name, player1.color)
-        self.board[7][0] = Rook((7, 0), player2.name, player2.color)
-        self.board[7][7] = Rook((7, 7), player2.name, player2.color)
-        self.board[0][1] = Knight((0, 1), player1.name, player1.color)
-        self.board[0][6] = Knight((0, 6), player1.name, player1.color)
-        self.board[7][1] = Knight((7, 1), player2.name, player2.color)
-        self.board[7][6] = Knight((7, 6), player2.name, player2.color)
-        self.board[0][2] = Bishop((0, 2), player1.name, player1.color)
-        self.board[0][5] = Bishop((0, 5), player1.name, player1.color)
-        self.board[7][2] = Bishop((7, 2), player2.name, player2.color)
-        self.board[7][5] = Bishop((7, 5), player2.name, player2.color)
-        self.board[0][3] = Queen((0, 3), player1.name, player1.color)
-        self.board[7][3] = Queen((7, 3), player2.name, player2.color)
-        self.board[0][4] = King((0, 4), player1.name, player1.color)
-        self.board[7][4] = King((7, 4), player2.name, player2.color)
+            self.board[1][i] = pieces.Pawn((1, i), player1.name, player1.color)
+            self.board[6][i] = pieces.Pawn((6, i), player2.name, player2.color)
+
+        self.board[0][0] = pieces.Rook((0, 0), player1.name, player1.color)
+        self.board[0][7] = pieces.Rook((0, 7), player1.name, player1.color)
+        self.board[7][0] = pieces.Rook((7, 0), player2.name, player2.color)
+        self.board[7][7] = pieces.Rook((7, 7), player2.name, player2.color)
+        self.board[0][1] = pieces.Knight((0, 1), player1.name, player1.color)
+        self.board[0][6] = pieces.Knight((0, 6), player1.name, player1.color)
+        self.board[7][1] = pieces.Knight((7, 1), player2.name, player2.color)
+        self.board[7][6] = pieces.Knight((7, 6), player2.name, player2.color)
+        self.board[0][2] = pieces.Bishop((0, 2), player1.name, player1.color)
+        self.board[0][5] = pieces.Bishop((0, 5), player1.name, player1.color)
+        self.board[7][2] = pieces.Bishop((7, 2), player2.name, player2.color)
+        self.board[7][5] = pieces.Bishop((7, 5), player2.name, player2.color)
+        self.board[0][3] = pieces.Queen((0, 3), player1.name, player1.color)
+        self.board[7][3] = pieces.Queen((7, 3), player2.name, player2.color)
+        self.board[0][4] = pieces.King((0, 4), player1.name, player1.color)
+        self.board[7][4] = pieces.King((7, 4), player2.name, player2.color)
 
 
+# TODO: Refactor this class
 class Team(Board):
     """Dummy piece class, to be edited later and as required"""
 
     def __init__(self, player1: str, player2: str) -> None:
-        self.player1 = PlayerAttributes(player1, "white")
-        self.player2 = PlayerAttributes(player2, "black")
+        self.player1 = data.PlayerAttributes(player1, "white")
+        self.player2 = data.PlayerAttributes(player2, "black")
         super().__init__()
         self.generate_pieces(self.player1, self.player2)
