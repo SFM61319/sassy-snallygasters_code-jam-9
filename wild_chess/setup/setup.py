270d9f8614d58ba51f3b9d14597e6ff9c544d961
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 01:24:41 +0530

    Rename `db` attribute to `database` and fix errors

--- wild_chess/setup/setup.py
+++ wild_chess/setup/setup.py
@@ -1,24 +1,25 @@
+"""Database setup."""
+
+
 import asyncio
 import os
 
 import asyncpg
 
-from wild_chess.database import PlayerDB
+from wild_chess.database import db
 
 
-class Setup:  # setup class
-    """
-    Class methods for setup.
-    """
+class Setup:
+    """Class methods for setup."""
 
-    pool: asyncpg.pool.Pool  # asyncpg pool
-    db: PlayerDB  # database
+    pool: asyncpg.pool.Pool
+    database: db.PlayerDB
 
-    async def set_db(self) -> None:  # connect to database
-        """ "
-        Set the database.
-        Uses asyncpg to connect to the database.
-        Fetches keys from the environment.
+    async def connect(self) -> None:
+        """
+        Connect to and set the database.
+
+        Uses asyncpg to connect to the database. Fetches keys from the environment.
         """
         self.pool = await asyncpg.create_pool(
             user=os.getenv("PGUSER"),
@@ -30,7 +31,9 @@ class Setup:  # setup class
             loop=asyncio.get_event_loop(),
         )
 
-    async def setup(self) -> None:  # setup to set all connections
-        await self.set_db()
-        self.db = PlayerDB()
-        await self.db.set_table(self.pool)
+    async def setup(self) -> None:
+        """Set up all connections"""
+        await self.connect()
+
+        self.database = db.PlayerDB()
+        await self.database.set_table(self.pool)
