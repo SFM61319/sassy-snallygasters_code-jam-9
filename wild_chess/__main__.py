5e24fc5392637bfb4a0d6775731f2d33a5b80700
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 01:20:17 +0530

    Fix linter errors in `wild_chess/__main__.py`

--- wild_chess/__main__.py
+++ wild_chess/__main__.py
@@ -6,23 +6,24 @@ import asyncio
 from wild_chess.setup import Setup
 
 
-async def test(setup) -> None:  # creating a function to test the setup
+async def test(setup: Setup) -> None:
+    """Tests setup"""
     await setup.setup()
-    await setup.db.create_player("test")
-    print(await setup.db.find_player("test"))
-    print(await setup.db.all_players())
-    await setup.db.update_player_win("test")
-    print(await setup.db.find_player("test"))
-    await setup.db.update_player_loss("test")
-    print(await setup.db.find_player("test"))
-    await setup.db.update_player_tie("test")
-    print(await setup.db.find_player("test"))
-    await setup.db.update_player_history("test", "user", ["00", "01", "02"], "win")
-    print(await setup.db.find_player("test"))
-    await setup.db.remove_player("test")
-    print(await setup.db.all_players())
-    await setup.db.drop_table()
-    print("sucess")
+    await setup.database.create_player("test")
+    print(await setup.database.find_player("test"))
+    print(await setup.database.all_players())
+    await setup.database.update_player_win("test")
+    print(await setup.database.find_player("test"))
+    await setup.database.update_player_loss("test")
+    print(await setup.database.find_player("test"))
+    await setup.database.update_player_tie("test")
+    print(await setup.database.find_player("test"))
+    await setup.database.update_player_history("test", "user", ["00", "01", "02"], "win")
+    print(await setup.database.find_player("test"))
+    await setup.database.remove_player("test")
+    print(await setup.database.all_players())
+    await setup.database.drop_table()
+    print("success")
 
 
 if __name__ == "__main__":
