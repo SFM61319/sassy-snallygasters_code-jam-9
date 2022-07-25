7df0e00115e99278be5f13a7db23d571981d4e9d
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 01:26:14 +0530

    Refactor the module

--- wild_chess/database/db.py
+++ wild_chess/database/db.py
@@ -1,29 +1,33 @@
-from __future__ import annotations
+"""Database module."""
 
-from typing import List, Optional, Tuple
+
+from __future__ import annotations
+import typing
 
 import asyncpg
 
-from ..utils import PlayerRecord
-from .postgres import DatabaseModel
-
-__all__: Tuple[str, ...] = ("PlayerDB",)
+from wild_chess.utils import data
+from wild_chess.database import postgres
 
 
-class PlayerDB(DatabaseModel):  # subclass of DatabaseModel for player data
-    """
-    all abstract methods for using player database.
-    """
+__all__: tuple[str, ...] = ("PlayerDB",)
+
+
+class PlayerDB(postgres.DatabaseModel):
+    """All abstract methods for using player database."""
 
     pool: asyncpg.pool.Pool
 
     async def set_table(self, pool: asyncpg.pool.Pool) -> None:
         """
         Set the database connection pool. And create the table if it doesn't exist.
+
         :param pool:
-        :return:
+        :type pool: asyncpg.pool.Pool
         """
         self.pool = pool
+
+        # TODO: Use a better primary key field
         await self.exec_write_query(
             """CREATE TABLE IF NOT EXISTS players(
                                        name TEXT PRIMARY KEY,
@@ -34,109 +38,135 @@ class PlayerDB(DatabaseModel):  # subclass of DatabaseModel for player data
         )
 
     async def drop_table(self) -> None:
-        """
-        Drop the table.
-        :return:
-        """
+        """Drop the table."""
         await self.exec_write_query("DROP TABLE players")
 
-    async def find_player(self, user_name: str) -> Optional[PlayerRecord]:
+    async def find_player(self, username: str) -> typing.Optional[data.PlayerRecord]:
         """
         Find a player by name.
-        :param user_name:
-        :return:
-        """
-        record = await self.exec_fetchone("SELECT * FROM players WHERE name = $1", (user_name,))
-        return PlayerRecord(*record) if record else None
 
-    async def create_player(self, user_name: str) -> Optional[PlayerRecord] | bool:
+        :param username:
+        :type username: str
+        :return:
+        :rtype: typing.Optional[data.PlayerRecord]
+        """
+        record = await self.exec_fetchone("SELECT * FROM players WHERE name = $1", (username,))
+        return data.PlayerRecord(*record) if record else None
+
+    async def create_player(self, username: str) -> typing.Optional[data.PlayerRecord] | bool:
         """
         Create a new player.
-        :param user_name:
+
+        :param username:
+        :type username: str
         :return:
+        :rtype: typing.Optional[data.PlayerRecord] | bool
         """
-        player = await self.find_player(user_name)
+        player = await self.find_player(username)
+
         if player is not None:
             return False
+
         await self.exec_write_query(
             "INSERT INTO players VALUES ($1, 0, 0, 0, $2)",
             (
-                user_name,
+                username,
                 [],
             ),
         )
-        return await self.find_player(user_name)
+        return await self.find_player(username)
 
-    async def remove_player(self, user_name: str) -> bool:
+    async def try_exec_write_query(self, username: str, query: str, query_data: typing.Optional[tuple] = None) -> bool:
+        """
+        Tries to execute a write query. Returns True if player with *username* exists, False otherwise.
+
+        :param username: The username of the player
+        :type username: str
+        :param query: The write query to execute
+        :type query: str
+        :param query_data: The variables to replace in the query (if any), defaults to None
+        :type query_data: typing.Optional[tuple], optional
+        :return: True if player exists, False otherwise.
+        :rtype: bool
+        """
+        player = await self.find_player(username)
+
+        if player is None:
+            return False
+
+        await self.exec_write_query(query, query_data)
+        return True
+
+    async def remove_player(self, username: str) -> bool:
         """
         Remove a player.
-        :param user_name:
-        :return:
-        """
-        player = await self.find_player(user_name)
-        if player is None:
-            return False
-        await self.exec_write_query("DELETE FROM players WHERE name = $1", (user_name,))
-        return True
 
-    async def update_player_win(self, user_name: str) -> bool:
+        :param username:
+        :type username:
+        :return:
+        :rtype: bool
+        """
+        return await self.try_exec_write_query(username, "DELETE FROM players WHERE name = $1", (username,))
+
+    async def update_player_win(self, username: str) -> bool:
         """
         Update a player's win.
-        :param user_name:
-        :return:
-        """
-        player = await self.find_player(user_name)
-        if player is None:
-            return False
-        await self.exec_write_query("UPDATE players SET wins = wins + 1 WHERE name = $1", (user_name,))
-        return True
 
-    async def update_player_loss(self, user_name: str) -> bool:
+        :param username:
+        :type username: str
+        :return:
+        :rtype: bool
+        """
+        return await self.try_exec_write_query(username, "UPDATE players SET wins = wins + 1 WHERE name = $1", (username,))
+
+    async def update_player_loss(self, username: str) -> bool:
         """
         Update a player's loss.
-        :param user_name:
-        :return:
-        """
-        player = await self.find_player(user_name)
-        if player is None:
-            return False
-        await self.exec_write_query("UPDATE players SET losses = losses + 1 WHERE name = $1", (user_name,))
-        return True
 
-    async def update_player_tie(self, user_name: str) -> bool:
+        :param username:
+        :type username: str
+        :return:
+        :rtype: bool
+        """
+        return await self.try_exec_write_query(username, "UPDATE players SET losses = losses + 1 WHERE name = $1", (username,))
+
+    async def update_player_tie(self, username: str) -> bool:
         """
         Update a player's tie.
-        :param user_name:
-        :return:
-        """
-        player = await self.find_player(user_name)
-        if player is None:
-            return False
-        await self.exec_write_query("UPDATE players SET ties = ties + 1 WHERE name = $1", (user_name,))
-        return True
 
-    async def update_player_history(self, user_name: str, player_name: str, moves: list, status: str) -> bool:
+        :param username:
+        :type username: str
+        :return:
+        :rtype bool:
+        """
+        return await self.try_exec_write_query(username, "UPDATE players SET ties = ties + 1 WHERE name = $1", (username,))
+
+    async def update_player_history(self, username: str, player_name: str, moves: list, status: str) -> bool:
         """
         Update a player's history.
-        :param user_name:
+
+        :param username:
+        :type username: str
         :param player_name:
+        :type player_name: str
         :param moves:
+        :type moves: list
         :param status:
+        :type status: str
         :return:
+        :rtype: bool
         """
-        player = await self.find_player(user_name)
+        player = await self.find_player(username)
+
         if player is None:
             return False
-        history = player.history
-        container = {"opponent": player_name, "moves": moves, "status": status}
-        history.append(str(container))
-        await self.exec_write_query("UPDATE players SET history = $1 WHERE name = $2", (history, user_name))
+
+        player.history.append(str({"opponent": player_name, "moves": moves, "status": status}))
+
+        await self.exec_write_query("UPDATE players SET history = $1 WHERE name = $2", (player.history, username))
         return True
 
-    async def all_players(self) -> List[PlayerRecord]:
-        """
-        Get all players.
-        :return:
-        """
+    async def all_players(self) -> list[data.PlayerRecord]:
+        """Get all players."""
         records = await self.exec_fetchall("SELECT * FROM players")
-        return [PlayerRecord(*record) for record in records]
+        return [data.PlayerRecord(*record) for record in records]
