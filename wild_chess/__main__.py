b2c38d6588f42a85aeb8a0cdd1fc56e6f5674372
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-26 17:07:59 +0530

    Fix linter errors in `wild_chess`

--- wild_chess/__main__.py
+++ wild_chess/__main__.py
@@ -3,29 +3,29 @@
 
 import asyncio
 
-from wild_chess.setup import Setup
+from wild_chess.setup import setup
 
 
-async def test(setup: Setup) -> None:
+async def test(test_setup: setup.Setup) -> None:
     """Tests setup"""
-    await setup.setup()
-    await setup.database.create_player("test")
-    print(await setup.database.find_player("test"))
-    print(await setup.database.all_players())
-    await setup.database.update_player_win("test")
-    print(await setup.database.find_player("test"))
-    await setup.database.update_player_loss("test")
-    print(await setup.database.find_player("test"))
-    await setup.database.update_player_tie("test")
-    print(await setup.database.find_player("test"))
-    await setup.database.update_player_history("test", "user", ["00", "01", "02"], "win")
-    print(await setup.database.find_player("test"))
-    await setup.database.remove_player("test")
-    print(await setup.database.all_players())
-    await setup.database.drop_table()
+    await test_setup.setup()
+    await test_setup.database.create_player("test")
+    print(await test_setup.database.find_player("test"))
+    print(await test_setup.database.all_players())
+    await test_setup.database.update_player_win("test")
+    print(await test_setup.database.find_player("test"))
+    await test_setup.database.update_player_loss("test")
+    print(await test_setup.database.find_player("test"))
+    await test_setup.database.update_player_tie("test")
+    print(await test_setup.database.find_player("test"))
+    await test_setup.database.update_player_history("test", "user", ["00", "01", "02"], "win")
+    print(await test_setup.database.find_player("test"))
+    await test_setup.database.remove_player("test")
+    print(await test_setup.database.all_players())
+    await test_setup.database.drop_table()
     print("success")
 
 
 if __name__ == "__main__":
-    bot = Setup()  # creating a bot object
+    bot = setup.Setup()  # creating a bot object
     asyncio.run(test(bot))  # calling the test function
