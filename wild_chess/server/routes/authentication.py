48c05e088ea5e44781c7a3426dba5652199c9c6b
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-29 17:55:15 +0530

    Fix imports and docs, and update routes

--- wild_chess/server/routes/authentication.py
+++ wild_chess/server/routes/authentication.py
@@ -1,33 +1,34 @@
 """contains authentication functions"""
 
-from fastapi import APIRouter
 
-from wild_chess.database.db import PlayerDB
+import fastapi
 
-route = APIRouter()
+from wild_chess.database import db
+
+route = fastapi.APIRouter()
 
 
-@route.post("/create_user")
-async def create_user(username: str, password: str) -> dict:
+@route.post("/signup")
+async def signup(username: str, password: str) -> dict:
     """
-    Creates user
+    Signs a user up.
 
     :param username:
     :param password:
     :return:
     """
-    player = await PlayerDB().create_player(username, password)
+    player = await db.PlayerDB().create_player(username, password)
     return {"message": "user_created"} if player else {"message": "username already exists"}
 
 
 @route.post("/login")
 async def login(username: str, password: str) -> dict:
     """
-    For login
+    Logs the user in.
 
     :param username:
     :param password:
     :return:
     """
-    player = await PlayerDB().check_password(username, password)
+    player = await db.PlayerDB().check_password(username, password)
     return {"message": "login_successful"} if player else {"message": "login_failed"}
