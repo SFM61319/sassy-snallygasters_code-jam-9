b79155e483a99d65e742b096dca6c9c5591babf0
Chara98 <70572923+Chara98@users.noreply.github.com>
2022-07-31 23:34:31 +0530

    removed errors

--- wild_chess/__main__.py
+++ wild_chess/__main__.py
@@ -1,9 +1,7 @@
 """Main file."""
 
 
-from wild_chess.server import main
 from wild_chess.utils import board
 
 if __name__ == "__main__":
     game = board.Board.start("Asher", "Ninja")
-    main.main()  # its a blocking function for trial purpose keep below till we shift game in server
