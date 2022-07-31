1ee4e9f2d77f283e9acd5d72725307ebfdf0d1ee
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-07-31 20:16:27 +0530

    fix db for post and get methods

--- wild_chess/server/main.py
+++ wild_chess/server/main.py
@@ -3,7 +3,7 @@
 import fastapi
 import uvicorn
 
-from .routes import authentication, leaderboard, multiplayer
+from wild_chess.server.routes import authentication, leaderboard, multiplayer
 
 app = fastapi.FastAPI()
 
