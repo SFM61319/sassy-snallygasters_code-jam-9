1ea982ff1bac1f961876b2f68b0a9948afe4003e
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2022-07-31 23:09:39 +0530

    Refactor code

--- wild_chess/server/routes/multiplayer.py
+++ wild_chess/server/routes/multiplayer.py
@@ -1,67 +1,90 @@
 """multiplayer functions"""
 
-from fastapi import APIRouter
-from random import shuffle
+
+import random
+
+import fastapi
+
+route = fastapi.APIRouter()
+active_game: dict = {}
 
 
-route = APIRouter()
-active_game = {}
-
-
-@route.post('/host-game')
-async def host_game(username: str) -> dict:
+@route.post("/host-game")
+async def host_game(username: str) -> dict[str, list[str]]:
     """
+    Hosts the game.
+
     :param username:
+    :type username: str
     :return:
+    :rtype: dict[str, list[str]]
     """
-    code: list[str] = [str(i) for i in range(6)]
-    shuffle(code)
-    shuffled: str = "".join(code)
-    active_game[username] = {'code': shuffled, 'board': None, 'players': [username]}
-    return {'code': code}
+    code = ['0', '1', '2', '3', '4', '5']
+
+    random.shuffle(code)
+    shuffled = "".join(code)
+
+    active_game[username] = {"code": shuffled, "board": None, "players": [username]}
+    return {"code": code}
 
 
-@route.post('/join-game/{code}')
-async def join_game(code: str, username: str) -> dict:
+@route.post("/join-game/{code}")
+async def join_game(code: str, username: str) -> dict[str, str]:
     """
+    Joins the game.
+
     :param code:
+    :type code: str
     :param username:
+    :type username: str
     :return:
+    :rtype: dict[str, str]
     """
-    for k, v in active_game.items():
-        if v['code'] == code:
-            if len(v['players']) == 1 and username not in v['players']:
-                v['players'].append(username)
-                return {'message': 'ok'}
+    for v in active_game.values():
+        if v["code"] == code:
+            if len(v["players"]) == 1 and username not in v["players"]:
+                v["players"].append(username)
+                return {"message": "ok"}
 
-    return {'message': 'error'}
+    return {"message": "error"}
 
 
-@route.post('/game/{code}')
-async def post_game(code: str, username: str, board: list[list[str]]) -> dict:
+@route.post("/game/{code}")
+async def post_game(code: str, username: str, board: list[list[str]]) -> dict[str, str]:
     """
+    Posts the game.
+
     :param code:
+    :type code: str
     :param username:
+    :type username: str
     :param board:
+    :type board: list[list[str]]
     :return:
+    :rtype: dict[str, str]
     """
-    for i in active_game:
-        if active_game[i]['code'] == code and username in active_game[i]['players']:
-            active_game[i]['board'] = board
-            return {'message': 'ok'}
+    for v in active_game.values():
+        if v["code"] == code and username in v["players"]:
+            v["board"] = board
+            return {"message": "ok"}
 
-    return {'message': 'error'}
+    return {"message": "error"}
 
 
-@route.get('/game/{code}')
-async def get_board(code: str, username: str) -> dict:
+@route.get("/game/{code}")
+async def get_board(code: str, username: str) -> dict[str, str | list[list[str]]]:
     """
+    Gets the board.
+
     :param code:
+    :type code: str
     :param username:
+    :type username: str
     :return:
+    :rtype: dict[str, str | list[list[str]]]
     """
-    for i in active_game:
-        if active_game[i]['code'] == code and username in active_game[i]['players']:
-            return {'board': active_game[i]['board']}
+    for v in active_game.values():
+        if v["code"] == code and username in v["players"]:
+            return {"board": v["board"]}
 
-    return {'board': 'error'}
+    return {"board": "error"}
