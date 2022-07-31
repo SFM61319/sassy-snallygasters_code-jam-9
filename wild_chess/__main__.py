c51e9a03973d3b9c7a47fe59c06e19744f28781e
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-07-31 19:40:00 +0530

    made start method in main.py

--- wild_chess/__main__.py
+++ wild_chess/__main__.py
@@ -1,12 +1,6 @@
 """Main file."""
 
-import asyncio
-#from wild_chess.server import server
-from wild_chess.gui import gui
 from wild_chess.utils import board
 
 if __name__ == "__main__":
-    game = gui.Game()
-    gameBoard = board.Board('wite', 'blak')
-    gameBoard.generate_pieces(gameBoard.player1, gameBoard.player2)
-    game.init(gameBoard.board)
+    game = board.Board.start('Asher', 'Ninja')
